bdf340ed2b2b14d24b36185421c9f750a659bf0c
星如雨 <liyun200104@163.com>
2023-10-10 09:38:41 +0800

    ✨ feat: learn github-workflows

--- .vitepress/cache/deps/xing-ly.js
+++ .vitepress/cache/deps/xing-ly.js
@@ -1,27 +1,32 @@
 import {
+  Fragment,
   createBaseVNode,
+  createBlock,
   createCommentVNode,
   createElementBlock,
   defineComponent,
+  normalizeClass,
   normalizeStyle,
   onMounted,
   openBlock,
   ref,
+  renderList,
   toDisplayString,
-  watch
-} from "./chunk-4KCUKNDK.js";
+  watch,
+  withModifiers
+} from "./chunk-OKFY4OVN.js";
 
-// node_modules/.pnpm/xing-ly@0.0.4/node_modules/xing-ly/dist/xing-ly.es.js
-var k = { class: "lx-canvas" };
-var A = ["width", "height"];
-var B = {
+// node_modules/.pnpm/xing-ly@0.0.13_@iconify-json+mdi@1.1.54_@unocss+reset@0.55.7_unocss@0.55.7_vite@4.4.9/node_modules/xing-ly/dist/xing-ly.es.js
+var G = { class: "lx-canvas" };
+var J = ["width", "height"];
+var O = {
   key: 0,
   class: "canvas-info"
 };
-var F = createBaseVNode("td", null, "图片路径", -1);
-var L = createBaseVNode("td", null, "缩放比例", -1);
-var U = createBaseVNode("td", null, "最小限制缩放", -1);
-var E = defineComponent({
+var V = createBaseVNode("td", null, "图片路径", -1);
+var T = createBaseVNode("td", null, "缩放比例", -1);
+var W = createBaseVNode("td", null, "最小限制缩放", -1);
+var Z = defineComponent({
   __name: "index",
   props: {
     imageUrl: {
@@ -48,8 +53,8 @@ var E = defineComponent({
       default: 0.1
     }
   },
-  setup(i) {
-    const m = i, a = ref(), u = ref(), g = ref(), e = ref({
+  setup(m) {
+    const v = m, i = ref(), n = ref(), h = ref(), t = ref({
       imgPos: { x: 0, y: 0 },
       // 图片定位
       lastMousePos: { x: 0, y: 0 },
@@ -62,134 +67,290 @@ var E = defineComponent({
       // 图片适应拉伸宽度
       imgYAutoScale: 0
       // 图片适应拉伸高度
-    }), s = ref({
+    }), o = ref({
       display: "none",
       top: "0px",
       left: "0px"
     });
-    function d() {
+    function S() {
       var l;
-      e.value.imgScale = 1, e.value.imgPos.x = 0, e.value.imgPos.y = 0, g.value = (l = a.value) == null ? void 0 : l.getContext("2d"), w();
+      t.value.imgScale = 1, t.value.imgPos.x = 0, t.value.imgPos.y = 0, h.value = (l = i.value) == null ? void 0 : l.getContext("2d"), p();
     }
-    function w() {
-      if (!a.value)
+    function p() {
+      if (!i.value)
         throw "canvas is null or undefined";
-      u.value = new Image(), u.value.src = m.imageUrl, u.value.onload = function() {
-        if (!a.value)
+      n.value = new Image(), n.value.src = v.imageUrl, n.value.onload = function() {
+        if (!i.value)
           throw "canvas is null or undefined";
-        e.value.imgXAutoScale = a.value.width / u.value.width, e.value.imgYAutoScale = a.value.height / u.value.height, r();
+        t.value.imgXAutoScale = i.value.width / n.value.width, t.value.imgYAutoScale = i.value.height / n.value.height, M();
       };
     }
-    function r() {
-      if (!a.value)
+    function M() {
+      if (!i.value)
         throw "canvas not defined";
-      g.value.clearRect(0, 0, a.value.width, a.value.height), g.value.drawImage(
-        u.value,
+      h.value.clearRect(0, 0, i.value.width, i.value.height), h.value.drawImage(
+        n.value,
         // 规定要使用的图像、画布或视频。
         0,
         0,
         // 开始剪切的 x 坐标位置。
-        u.value.width,
-        u.value.height,
+        n.value.width,
+        n.value.height,
         // 被剪切图像的高度。
-        e.value.imgPos.x,
-        e.value.imgPos.y,
+        t.value.imgPos.x,
+        t.value.imgPos.y,
         // 在画布上放置图像的x、y坐标位置。
-        u.value.width * e.value.imgScale * e.value.imgXAutoScale,
-        u.value.height * e.value.imgScale * e.value.imgYAutoScale
+        n.value.width * t.value.imgScale * t.value.imgXAutoScale,
+        n.value.height * t.value.imgScale * t.value.imgYAutoScale
         // 要使用的图像的宽度、高度
       );
     }
-    function S(l) {
-      if (l.preventDefault(), e.value.dragging) {
-        const n = h(l.clientX, l.clientY);
-        e.value.imgPos.x += n.x - e.value.lastMousePos.x, e.value.imgPos.y += n.y - e.value.lastMousePos.y, e.value.lastMousePos = JSON.parse(JSON.stringify(n)), r();
+    function b(l) {
+      if (l.preventDefault(), t.value.dragging) {
+        const s = _(l.clientX, l.clientY);
+        t.value.imgPos.x += s.x - t.value.lastMousePos.x, t.value.imgPos.y += s.y - t.value.lastMousePos.y, t.value.lastMousePos = JSON.parse(JSON.stringify(s)), M();
       }
     }
-    function p(l) {
-      l.preventDefault(), s.value.display = "none", e.value.dragging = true, e.value.lastMousePos = h(l.clientX, l.clientY);
+    function k(l) {
+      l.preventDefault(), o.value.display = "none", t.value.dragging = true, t.value.lastMousePos = _(l.clientX, l.clientY);
     }
-    function P() {
-      e.value.dragging = false;
+    function w() {
+      t.value.dragging = false;
     }
-    function _() {
-      e.value.dragging = false;
+    function $() {
+      t.value.dragging = false;
     }
-    function M(l) {
+    function A(l) {
       l.preventDefault();
-      var n = h(l.clientX, l.clientY);
-      const c = l.wheelDelta ? l.wheelDelta : l.deltaY * -40;
-      var o = {
-        x: Number(((n.x - e.value.imgPos.x) / e.value.imgScale).toFixed(2)),
-        y: Number(((n.y - e.value.imgPos.y) / e.value.imgScale).toFixed(2))
+      var s = _(l.clientX, l.clientY);
+      const x = l.wheelDelta ? l.wheelDelta : l.deltaY * -40;
+      var e = {
+        x: Number(((s.x - t.value.imgPos.x) / t.value.imgScale).toFixed(2)),
+        y: Number(((s.y - t.value.imgPos.y) / t.value.imgScale).toFixed(2))
       };
-      c > 0 ? e.value.imgScale += 0.1 : (e.value.imgScale -= 0.1, e.value.imgScale < m.minLimitScale && (e.value.imgScale = m.minLimitScale)), e.value.imgPos.x = (1 - e.value.imgScale) * o.x + (n.x - o.x), e.value.imgPos.y = (1 - e.value.imgScale) * o.y + (n.y - o.y), r();
+      x > 0 ? t.value.imgScale += 0.1 : (t.value.imgScale -= 0.1, t.value.imgScale < v.minLimitScale && (t.value.imgScale = v.minLimitScale)), t.value.imgPos.x = (1 - t.value.imgScale) * e.x + (s.x - e.x), t.value.imgPos.y = (1 - t.value.imgScale) * e.y + (s.y - e.y), M();
     }
-    function h(l, n) {
-      var o;
-      if (!a.value)
+    function _(l, s) {
+      var e;
+      if (!i.value)
         throw "canvas is null or undefined";
-      var c = (o = a.value) == null ? void 0 : o.getBoundingClientRect();
+      var x = (e = i.value) == null ? void 0 : e.getBoundingClientRect();
       return {
-        x: l - c.left - (c.width - a.value.width) / 2,
-        y: n - c.top - (c.height - a.value.height) / 2
+        x: l - x.left - (x.width - i.value.width) / 2,
+        y: s - x.top - (x.height - i.value.height) / 2
       };
     }
     function N(l) {
-      l.preventDefault(), s.value.display = "block", s.value.top = `${l.clientY}px`, s.value.left = `${l.clientX}px`;
+      l.preventDefault(), o.value.display = "block", o.value.top = `${l.clientY}px`, o.value.left = `${l.clientX}px`;
     }
-    function b() {
-      d(), s.value.display = "none";
+    function C() {
+      S(), o.value.display = "none";
     }
-    return watch(m, () => {
-      d();
+    return watch(v, () => {
+      S();
     }), onMounted(() => {
-      d();
-    }), (l, n) => (openBlock(), createElementBlock("div", k, [
+      S();
+    }), (l, s) => (openBlock(), createElementBlock("div", G, [
       createBaseVNode("canvas", {
         ref_key: "canvas",
-        ref: a,
-        width: i.width,
-        height: i.height,
-        onMousedown: p,
-        onMousemove: S,
-        onMouseout: _,
-        onMouseup: P,
-        onWheel: M,
+        ref: i,
+        width: m.width,
+        height: m.height,
+        onMousedown: k,
+        onMousemove: b,
+        onMouseout: $,
+        onMouseup: w,
+        onWheel: A,
         onContextmenu: N
-      }, null, 40, A),
-      i.showInfo ? (openBlock(), createElementBlock("div", B, [
+      }, null, 40, J),
+      m.showInfo ? (openBlock(), createElementBlock("div", O, [
         createBaseVNode("table", null, [
           createBaseVNode("tr", null, [
-            F,
-            createBaseVNode("td", null, toDisplayString(i.imageUrl), 1)
+            V,
+            createBaseVNode("td", null, toDisplayString(m.imageUrl), 1)
           ]),
           createBaseVNode("tr", null, [
-            L,
-            createBaseVNode("td", null, toDisplayString(Number(e.value.imgScale).toFixed(2)), 1)
+            T,
+            createBaseVNode("td", null, toDisplayString(Number(t.value.imgScale).toFixed(2)), 1)
           ]),
           createBaseVNode("tr", null, [
-            U,
-            createBaseVNode("td", null, toDisplayString(Number(i.minLimitScale).toFixed(2)), 1)
+            W,
+            createBaseVNode("td", null, toDisplayString(Number(m.minLimitScale).toFixed(2)), 1)
           ])
         ])
       ])) : createCommentVNode("", true),
       createBaseVNode("div", {
         class: "context-menu",
-        style: normalizeStyle(s.value)
+        style: normalizeStyle(o.value)
       }, [
-        createBaseVNode("div", { onClick: b }, "还原缩放")
+        createBaseVNode("div", { onClick: C }, "还原缩放")
       ], 4)
     ]));
   }
 });
-var O = {
-  install: function(i) {
-    i.component("xl-canvas-image", E);
+var q = {
+  key: 0,
+  class: "text-red i-mdi-flag"
+};
+var H = {
+  key: 0,
+  class: "i-mdi-mine"
+};
+var K = { key: 1 };
+var Q = defineComponent({
+  __name: "MineBlock",
+  props: {
+    block: {}
+  },
+  setup(m) {
+    const v = [
+      "text-transparent",
+      "text-blue-500",
+      "text-green-500",
+      "text-yellow-500",
+      "text-orange-500",
+      "text-red-500",
+      "text-purple-500",
+      "text-pink-500"
+    ];
+    function i(n) {
+      return n.flagged ? "bg-gray-500/10" : n.revealed ? n.mine ? "text-red" : v[n.adjacentMines] : "bg-gray-500/10 hover:bg-gray-500/20";
+    }
+    return (n, h) => (openBlock(), createElementBlock("button", {
+      class: normalizeClass([i(n.block), "border-1 border-gray-500/10"])
+    }, [
+      n.block.flagged ? (openBlock(), createElementBlock("div", q)) : n.block.revealed ? (openBlock(), createElementBlock(Fragment, { key: 1 }, [
+        n.block.mine ? (openBlock(), createElementBlock("div", H)) : (openBlock(), createElementBlock("div", K, toDisplayString(n.block.adjacentMines), 1))
+      ], 64)) : createCommentVNode("", true)
+    ], 2));
+  }
+});
+var ee = { class: "game-mines-map" };
+var te = defineComponent({
+  __name: "index",
+  setup(m) {
+    let v = false;
+    const i = ref(15), n = ref(0), h = ref("play"), t = ref("general"), o = {
+      simple: {
+        mineNum: 10,
+        width: 5,
+        height: 5
+      },
+      general: {
+        mineNum: 40,
+        width: 15,
+        height: 15
+      },
+      difficult: {
+        mineNum: 60,
+        width: 15,
+        height: 15
+      },
+      hell: {
+        mineNum: 80,
+        width: 15,
+        height: 15
+      }
+    }, S = [
+      [-1, 0],
+      [-1, -1],
+      [-1, 1],
+      [0, -1],
+      [0, 1],
+      [1, -1],
+      [1, 0],
+      [1, 1]
+    ], p = ref([]);
+    function M() {
+      h.value = "play", v = false, n.value = 0, i.value = o[t.value].mineNum, p.value = Array.from(
+        { length: o[t.value].height },
+        (e, a) => Array.from(
+          { length: o[t.value].width },
+          (u, r) => ({
+            x: r,
+            y: a,
+            adjacentMines: 0
+          })
+        )
+      );
+    }
+    function b(e) {
+      e.revealed || ((n.value === i.value && e.flagged || n.value < i.value) && (e.flagged = !e.flagged, n.value += e.flagged ? 1 : -1), C());
+    }
+    function k(e, a, u) {
+      n.value = 0, w(e, a, u), $(e);
+    }
+    function w(e, a, u) {
+      if (u === 0)
+        return;
+      const r = x(0, o[t.value].width), g = x(0, o[t.value].height);
+      Math.abs(a.x - r) <= 1 && Math.abs(a.y - g) <= 1 || e[g][r].mine ? w(e, a, u) : (e[g][r].mine = true, w(e, a, u - 1));
+    }
+    function $(e) {
+      e.forEach((a) => {
+        a.forEach((u) => {
+          u.mine || N(u).forEach((r) => {
+            r.mine && (u.adjacentMines += 1);
+          });
+        });
+      });
+    }
+    function A(e) {
+      h.value !== "play" || e.flagged || (e.revealed = true, v || (k(p.value, e, i.value), v = true), e.mine && (h.value = "lost", l()), _(e), C());
+    }
+    function _(e) {
+      e.adjacentMines || N(e).forEach((a) => {
+        a.revealed || (a.revealed = true, _(a));
+      });
+    }
+    function N(e) {
+      return S.map(([a, u]) => {
+        const r = e.x + a, g = e.y + u;
+        if (!(r < 0 || g < 0 || r >= o[t.value].width || g >= o[t.value].height))
+          return p.value[g][r];
+      }).filter(Boolean);
+    }
+    function C() {
+      if (!v || h.value !== "play")
+        return;
+      const e = p.value.flat();
+      (!e.some((a) => !a.mine && !a.revealed) || e.filter((a) => a.mine).every((a) => a.flagged)) && (h.value = "won");
+    }
+    function l() {
+      p.value.flat().forEach((e) => {
+        e.mine && (e.revealed = true);
+      });
+    }
+    function s(e, a) {
+      return Math.random() * (a - e) + e;
+    }
+    function x(e, a) {
+      return Math.floor(s(e, a));
+    }
+    return M(), (e, a) => (openBlock(), createElementBlock("div", ee, [
+      (openBlock(true), createElementBlock(Fragment, null, renderList(p.value, (u, r) => (openBlock(), createElementBlock("div", {
+        key: r,
+        class: "flex justify-center"
+      }, [
+        (openBlock(true), createElementBlock(Fragment, null, renderList(u, (g, Y) => (openBlock(), createBlock(Q, {
+          key: Y,
+          class: "h-10 w-10 flex items-center justify-center",
+          block: g,
+          onClick: (j) => A(g),
+          onContextmenu: withModifiers((j) => b(g), ["prevent"])
+        }, null, 8, ["block", "onClick", "onContextmenu"]))), 128))
+      ]))), 128))
+    ]));
+  }
+});
+var ne = {
+  install: function(m) {
+    m.component("xl-canvas-image", Z), m.component("xl-game-mines", te);
   }
 };
 export {
-  E as XlCanvasImage,
-  O as default
+  Z as XlCanvasImage,
+  te as XlGameMines,
+  ne as default
 };
 //# sourceMappingURL=xing-ly.js.map
---
+++ blogs/others/github-workflows.md
@@ -0,0 +1,48 @@
+---
+title: GitHub Actions
+layout: doc
+---
+# GitHub Actions
+
+<el-divider />
+<div style='display: flex;gap: 10px;'>
+  <el-tag>github</el-tag>
+  <el-tag>tools</el-tag>
+  <el-tag>automation</el-tag>
+</div>
+
+## 概述
+GitHub Actions 是一种持续集成和持续交付 (CI/CD) 平台，可用于自动执行生成、测试和部署管道。 您可以创建工作流程来构建和测试存储库的每个拉取请求，或将合并的拉取请求部署到生产环境。
+
+## GitHub Actions 的组件
+配置 GitHub Actions 工作流，使其在存储库中发生事件（例如打开拉取请求或创建问题）时触发 。 工作流包含一个或多个可按顺序或并行运行的作业。 每个作业都将在其自己的虚拟机运行器中或在容器中运行，并具有一个或多个步骤，用于运行定义的脚本或运行动作。动作是一个可重用的扩展，可简化工作流 。
+![](https://docs.github.com/assets/cb-25535/mw-1440/images/help/actions/overview-actions-simple.webp)
+
+## 工作流
+工作流程是一个可配置的自动化过程，它将运行一个或多个作业。 工作流程由签入到存储库的 YAML 文件定义，并在存储库中的事件触发时运行，也可以手动触发，或按定义的时间表触发。
+
+## 使用工作流
+```yaml
+# 定义工作流名称(自定义，非必须)
+name: test-github-action
+# 工作流触发事件(https://docs.github.com/zh/actions/using-workflows/events-that-trigger-workflows)
+on: [push]
+# 定义所有的作业项
+jobs:
+  # 定义作业项的 id(自定义)
+  test-github-action:
+    # 定义作业项的 名称(自定义)
+    name: test-github-action
+    # 指定运行所需要的虚拟机环境(必填)
+    runs-on: ubuntu-latest
+    # 指定每个 Job 的运行步骤，可以包含一个或多个步骤。每个步骤都可以指定以下三个字段。
+      # name：步骤名称。
+      # run：该步骤运行的命令或者 action。
+      # env：该步骤所需的环境变量。
+    steps:
+    - name: Print `hello world`
+      env:
+        HELLO: hello
+        WORLD: world
+      run: echo $HELLO $WORLD
+```
