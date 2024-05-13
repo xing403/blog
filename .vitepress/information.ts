9bd85f3dced96d4031462099befdc2d9307cd137
星如雨 <liyun200104@163.com>
2024-05-13 14:47:46 +0800

    公网安备

---
+++ .vitepress/information.ts
@@ -0,0 +1,6 @@
+export default {
+  copyright: 'Copyright &copy; 2020 - 2024 <a href="http://ilstudy.vip" target="_blank">星如雨</a>',
+  ICP: '<a href="https://beian.miit.gov.cn" target="_blank">豫ICP备2021004680号-1</a>',
+  mpsIcon: '<img src="/images/mps_logo.png" style="width: 16px;height: 16px;" >',
+  mps: '<a href="https://beian.mps.gov.cn/#/query/webSearch?code=41162402000245" rel="noreferrer" target="_blank">豫公网安备41162402000245 </a>'
+}
